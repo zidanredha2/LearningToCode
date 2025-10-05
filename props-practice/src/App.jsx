58a0d4b402996d0222fefa09e4ba4b9cc92fe519
zidanredha2 <amazinginflation58@gmail.com>
2025-10-05 20:26:12 +0530

    added css transform styling to the elements of props

--- props-practice/src/App.jsx
+++ props-practice/src/App.jsx
@@ -1,10 +1,11 @@
 import Student from "./Student.jsx"
-
+import './App.css'
 function App() {
   return (
     <>
-      <Student name="SpongeBob" age="19" isStudent={true}/>
-      <Student name="Patrick" age="21" isStudent={false}></Student>
+      <Student name="SpongeBob" age={12} isStudent={true}/>
+      <Student name="Patrick" age={22} isStudent={false}></Student>
+      <Student name= "Squidward" age={43}></Student>
     </>
   )
 }
--- props-practice/src/Student.jsx
+++ props-practice/src/Student.jsx
@@ -1,13 +1,25 @@
+import PropTypes from 'prop-types';
 function Student(props) {
     return (
         <>
             <div className="student">
-                <h1>Hello, {props.name}!</h1>
-                <h2>He is {props.age} years old!</h2>
-                <h2>Student: {props.isStudent ? "Yes" : "No"}</h2>
+                <div>
+                    <h1>Hello, {props.name}!</h1>
+                    <h2>He is {props.age} years old!</h2>
+                    <h2>Student: {props.isStudent ? "Yes" : "No"}</h2>
+                </div>
             </div>
         </>
     )
 }
-
+Student.PropTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
+}
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false
+}
 export default Student;
